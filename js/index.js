8b4c35fd1000410b2ad4efeeb639e2b3292aeaf4
tjleonc <to.leon@duocuc.cl>
2024-05-13 12:57:04 -0400

    se agregó fetch y json

--- js/index.js
+++ js/index.js
@@ -1,18 +1,31 @@
-const contenedorTarjetas = document.getElementById("productos-container");
+document.addEventListener('DOMContentLoaded', function() {
+  const productoContainer = document.getElementById("productos");
 
-/** Crea las tarjetas de productos teniendo como padre productos.js */
-function crearTarjetasProductosInicio(productos){
-  productos.forEach(producto => {
-    const nuevoProducto = document.createElement("div");
-    nuevoProducto.classList = "tarjeta-producto"
-    nuevoProducto.innerHTML = `
-    <img src="./img/productos/${producto.id}.png" alt="Neumaticos">
-    <img src="./img/productos/${producto.id}.jpg" alt="Llantas">
-    <h3>${producto.nombre}</h3>
-    <p class="precio">$${producto.precio}</p>
-    <button>Agregar al carrito</button>`
-    contenedorTarjetas.appendChild(nuevoProducto);
-    nuevoProducto.getElementsByTagName("button")[0].addEventListener("click",() => agregarAlCarrito(producto))
-  });
-}
-crearTarjetasProductosInicio(productos);
+  //cargar el archivo json
+  fetch('./js/productos.json')
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(producto => {
+        const card = document.createElement('div');
+        card.classList.add('tarjeta-producto');
+        card.innerHTML = `
+          <img src="./img/productos/${producto.id}.png" alt="Neumaticos">
+          <img src="./img/productos/${producto.id}.jpg" alt="Llantas">
+          <h3>${producto.nombre}</h3>
+          <p class="precio">$${producto.precio}</p>
+          <button>Agregar al carrito</button>
+        `;
+        productoContainer.appendChild(card);
+        card.getElementsByTagName("button")[0].addEventListener("click",() => agregarAlCarrito(producto))
+      });
+    })
+
+
+
+
+
+
+
+
+
+});
